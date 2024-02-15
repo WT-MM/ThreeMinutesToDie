60383624a87174b28fcf07a095221b414110b8c8
BryanGrisales <bryangrisales01@gmail.com>
2024-02-15 01:01:18 -0500

    animation set up and movement done, the character sprite is a little yanky which makes turn running really choppy

--- player.gd
+++ player.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
 
-@export var speed : float =  300.0
-@export var jump_velocity : float = -400.0
+@export var speed : float =  150
+@export var jump_velocity : float = -150
+
+@onready var animated_sprite : AnimatedSprite2D = $playerAnimation
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var animation_locked : bool = false
+var direction: Vector2 = Vector2.ZERO
 
 
 func _physics_process(delta):
@@ -14,15 +18,32 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_vector("Left", "Right", "Up", "Down")
 	if direction:
-		velocity.x = direction * speed
+		velocity.x = direction.x * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-
+		
 	move_and_slide()
+	update_animation()
+	update_facing_direction()
+ 
+
+func update_animation():
+	if not animation_locked:
+		if direction.x != 0:
+			animated_sprite.play("run")
+		else: 
+			animated_sprite.play("idle")
+			
+
+func update_facing_direction():
+	if direction.x > 0:
+		animated_sprite.flip_h = false
+	elif direction.x < 0:
+		animated_sprite.flip_h = true
