8d027e6eff0d1fba88c2d7b5e2a6ed80accbbc68
BryanGrisales <bryangrisales01@gmail.com>
2024-02-18 19:55:58 -0500

    Created attack state, but i need to speed up the animations by a bit to make it smoother, also considering removing attack 2 and just replacing it with 3. Next I will addthe hitbox for punches and hp interactions.

--- character/GroundState.gd
+++ character/GroundState.gd
@@ -4,6 +4,8 @@ class_name GroundState
 @export var jump_velocity : float = -150
 @export var air_state : State
 @export var jump_animation : String = "jump"
+@export var attack_state : State
+@export var attack_node: String = "attack_1"
 
 #velocity.y = jump_velocity
 
@@ -14,8 +16,14 @@ func state_process(delta):
 func state_input(event: InputEvent): 
 	if (event.is_action_pressed("Jump")): 
 		jump()
-		
+	if(event.is_action_pressed("Attack")):
+		attack()
+
 func jump():
 	character.velocity.y = jump_velocity
 	next_state = air_state
 	playback.travel(jump_animation)
+
+func attack():
+	next_state = attack_state
+	playback.travel(attack_node)
