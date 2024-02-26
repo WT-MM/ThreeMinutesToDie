60f8dead86e2e7aadfc709a485e5bca61057e93d
BryanGrisales <bryangrisales01@gmail.com>
2024-02-25 23:28:26 -0500

    added ingame timer to UI, this can and will probably be tweaked later but it exists temporarily so I can test end game conditions

---
+++ UI/gameTimer.gd
@@ -0,0 +1,24 @@
+extends Label
+
+@onready var timer : Timer = $Timer
+# Called when the node enters the scene tree for the first time.
+
+var s = 0
+var m = 3
+
+func _ready():
+	timer.start()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if (s < 0 ):
+		m -= 1
+		s = 59
+	
+	text = "Time: " + (str(m) + ":" + str(s))
+	
+
+
+func _on_timer_timeout():
+	s -= 1
