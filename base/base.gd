f276dc1ae2e88746398238d1feec01ac9f132ba2
BryanGrisales <bryangrisales01@gmail.com>
2024-02-25 19:28:50 -0500

    made it so the base is interactable with E instead of purely just on contact. There is also a hovering label to indicate the player must press E.

--- base/base.gd
+++ base/base.gd
@@ -1,17 +1,25 @@
 extends Area2D
 
+@onready var info_label: Label = $Label
+var player_in_area = false  # Flag to track whether the player is in the area
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	info_label.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
-
-func _on_body_entered(body):
-	if(body.name == "player"):
+	if player_in_area and Input.is_action_just_pressed("Interact"):
 		get_tree().paused = true
 		get_node("baseMenu").visible = true
 		get_node("baseMenu/AnimationPlayer").play("TransIn")
+
+func _on_body_entered(body):
+	if body.name == "player":
+		player_in_area = true
+		info_label.visible = true
+
+func _on_body_exited(body):
+	if body.name == "player":
+		player_in_area = false
+		info_label.visible = false
